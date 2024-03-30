5cc35ab6bc19b97972892c5c3965ee7eb583d2a1
Lang (Ron) Chen <Ronstuckathome@gmail.com>
2024-03-30 19:33:38 +1100

    fix bug

--- Source Code/JiaoChengB.py
+++ Source Code/JiaoChengB.py
@@ -351,7 +351,7 @@ class JiaoChengB:
 
             starting_hp_combo = [self.param_value_reverse_map[hp][self.hyperparameter_default_values[hp]] for hp in self.hyperparameters] # setup starting combination
 
-            if starting_hp_combo == 0:
+            if starting_feature_index == 0:
                 print('\nDefault combo:', starting_hp_combo, '\n')
 
             round = 1
